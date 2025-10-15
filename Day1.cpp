efa89b42ee66e7fc5d09bff2bc098ca0dc4423b0
rockcee31 <thapliyalrohit31@gmail.com>
2025-10-15 12:45:27 +0530

    Q2

--- Day1.cpp
+++ Day1.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
 // Q1) Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
 //  arr[] = [1, 2, 3, 4], x = 3
+// int search(vector<int>& arr,int ele){
+//     for(int i =0; i<arr.size();i++){
+//         if(arr[i] == ele){
+//             return i;
+//         }
+//     };
+//     return -1;
+// };
 
-int search(vector<int>& arr,int ele){
-    for(int i =0; i<arr.size();i++){
-        if(arr[i] == ele){
-            return i;
+
+
+// int main(){
+//     vector<int> nums  = {1,2,3,4,5};
+//     int x = 3;
+//     cout << "Result: " << search(nums, x);
+//     return 0;
+
+// };
+
+
+
+
+// Q2)Given two arrays a[] and b[] of equal size, the task is to find whether the elements in the arrays are equal.
+// Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though.
+
+bool EqualOrNot(vector<int>&a,vector<int>&b){
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        if(a==b){
+            return true;
         }
-    
-    };
-    return -1;
+        else{
+            return false;
+        };
 };
-
-
-
 int main(){
-    vector<int> nums  = {1,2,3,4,5};
-    int x = 3;
-    cout << "Result: " << search(nums, x);
-    return 0;
-
+    vector<int> a = {1,4,2,1,0};
+    vector<int> b = {2,1,1,4,0};
+     
+    cout<<boolalpha<<EqualOrNot(a,b);
 };
-
-
-
-
 
 
 
@@ -57,3 +75,22 @@ int main(){
 // Changes inside the function directly affect the original data.
 // No copy is made, making it memory and time efficient for large data structures.
 // Useful when you want the function to modify the input itself.
+
+// #include <bits/stdc++.h>
+// It’s a shortcut header used in competitive programming.
+
+// It automatically includes almost every standard C++ header:
+
+// <vector>
+
+// <algorithm>
+
+// <iostream>
+
+// <string>
+
+// <map>, <set>, etc.
+
+// That’s why sort() seems to “work without including <algorithm>” in those examples.
+
+// boolalpha is a stream manipulator from the <iostream> library that changes how booleans are displayed in the output stream. bool is always printed as 0 and 1  so to show it as true or false we should add bool aplha 
